12fcaa5f20cc6e37a01fafa3bab0c223ddd4a1ea
Nitesh <nitesh.e21@nsut.ac.in>
2025-05-13 19:07:31 +0530

    hbs

---
+++ lec-14-hbs/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app= express();
+const mongoose = require('mongoose');
+const Blog = require("./model/blog");
+const userRoutes= require("./routes/userRoutes")
+app.use(express.json());
+app.set('view engine', 'hbs');
+app.use("/users",userRoutes)
+app.use("/blogs",require("./routes/blogRoutes"))
+
+
+app.get("/",(req,res)=>{
+    res.render("home",{
+        name:"Nitesh"
+    })
+})
+
+app.get("/blogs",async(req,res)=>{
+    let allblogs= await Blog.find();
+    console.log(allblogs);
+    res.render("blogs",{
+        data:allblogs
+    });
+
+})
+
+
+
+
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/g14db')
+  .then(() => console.log('Connected!'))
+  .catch((err)=>console.log(err));
+app.listen(4433,(req,res)=>{
+    console.log("server started")
+})
